a9b87219770f1216857a2a03d3c92048293c8cfa
Issac Angulo <issac.angulo@gmail.com>
2020-09-02 06:45:57 -0700

    sequelize connection ok

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const logger = require('./middleare/logger');
+
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+    port: 3306,
+    host: process.env.HOST,
+    dialect: 'mysql'
+});
+
+
+general = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+general();
+
+var app = express();
+
+app.use(logger);
+
+app.get('/', (req, res) => res.send('INDEX'));
+
+var PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/interface.js
@@ -0,0 +1,70 @@
+//'use strict';
+/*const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+const env = process.env.NODE_ENV;
+const tarenv = require(__dirname + '/direction.json');
+const dev = Boolean(tarenv.dev.enabled);
+const prod = Boolean(tarenv.prod.enabled);
+var db = {};*/
+
+
+
+let sequelize = '';
+
+const dev = true;
+const prod = false;
+console.log(`${process.env.DATABASE},${process.env.USERNAME},${process.env.PASSWORD},${process.env.HOST}`);
+
+//DEV ENV ENABLED
+/*if(dev && !prod) {
+    sequelize = new Sequelize(
+        process.env.JAWSDB_AQUA_DATABASE,
+        process.env.JAWSDB_AQUA_USERNAME,
+        process.env.JAWSDB_AQUA_PASSWORD,
+        {
+            host: process.env.JAWSDB_AQUA_HOST,
+            dialect: 'mysql'
+        });
+//PROD ENV ENABLED
+} else if (!dev && prod) {
+    sequelize = new Sequelize(
+        process.env.JAWSDB_DATABASE,
+        process.env.JAWSDB_USERNAME,
+        process.env.JAWSDB_PASSWORD,
+        {
+            host: process.env.JAWSDB_HOST,
+            dialect: 'mysql'
+        });
+
+} else {
+    console.log('Interface.js NA');
+}*/
+
+//sequelize.authenticate().then(() => console.log('DB connected')).catch(err => console.log(`error ${err}`));
+
+/*config.host = process.env.HOST;
+config.database = process.env.DATABASE;
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.SQLPASS, config);
+
+fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(function(file) {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(function(modelName) {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;*/
+
+//module.exports = db;
