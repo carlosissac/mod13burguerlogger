012a3f80c609772e9578ed1570d0c31fc3a4d497
Issac Angulo <issac.angulo@gmail.com>
2020-09-06 19:47:51 -0700

    handlebars functionality completed

--- routes/public/pages.js
+++ routes/public/pages.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const app = express();
-const db = require('../../models');
+//const db = require('../../models');
 
 
 app.get('/', (req, res) => {
