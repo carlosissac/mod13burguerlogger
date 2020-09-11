e4d38bf534a218cb0d679f290f28752d1e857f34
Issac Angulo <issac.angulo@gmail.com>
2020-09-11 13:39:08 -0700

    heroku troubleshoot

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log(`${process.env.DATABASE}, ${process.env.USERNAME}, ${process.env.PASSWORD}, ${process.env.HOST}`);
+
 module.exports = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql'
