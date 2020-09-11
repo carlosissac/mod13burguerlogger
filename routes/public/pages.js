1f73c42fa69f8e23752d9eb0ff0586095f733cee
Issac Angulo <issac.angulo@gmail.com>
2020-09-11 12:42:45 -0700

    heroku troubleshoot

--- routes/public/pages.js
+++ routes/public/pages.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const { Internal } = require('../../helper/axios/internal');
 
-
-app.get('/', async (req, res) => {
+app.get('/', async function (req, res) {
     const internal = new Internal();
     let array = await internal.mainPageArray(req.protocol,req.get('host'),req.originalUrl);
     res.render('burger', { array });
 });
 
-app.get('*', (req, res) => {
+app.get('*', function (req, res) {
     res.render('notfound');
 });
 
