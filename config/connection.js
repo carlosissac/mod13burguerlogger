790c0878f857245703a1ecad773030001a049413
Issac Angulo <issac.angulo@gmail.com>
2020-09-11 11:04:45 -0700

    heroku login

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 module.exports = new Sequelize(`${process.env.DATABASE}`, `${process.env.USERNAME}`, `${process.env.PASSWORD}`, {
-    host: process.env.HOST,
+    host: `${process.env.HOST}`,
     dialect: 'mysql'
 });
 
