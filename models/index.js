7bd6ba94bd94559d0126133dd4f6a2333c5f0e78
Issac Angulo <issac.angulo@gmail.com>
2020-09-11 20:49:21 -0700

    heroku troubleshoot

--- models/index.js
+++ models/index.js
@@ -1,9 +1,25 @@
-const { Sequelize } = require('sequelize');
-const sequelize = require('../config/connection');
-const fs = require('fs');
-const path = require('path');
-const basename = path.basename(module.filename);
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+require('dotenv').config();
+var Sequelize = require('sequelize');
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
+
+///DEV VERSION
+/*var sequelize = '';
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}*/
+///HEROKKU VERSION
+config.host = process.env.HOST;
+config.database = process.env.DATABASE;
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, config);
 
 fs
     .readdirSync(__dirname)
@@ -11,6 +27,7 @@ fs
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(function(file) {
+        //var model = sequelize['import'](path.join(__dirname, file));
         const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
