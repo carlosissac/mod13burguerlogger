8ef7887af36a303aa6b5add52efbc6e3470fa47c
Issac Angulo <issac.angulo@gmail.com>
2020-09-11 23:25:21 -0700

    final revs

--- seed.sql
+++ seed.sql
@@ -1,16 +1,16 @@
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten) 
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten) 
 VALUES ('Burger One', TRUE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten)
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten)
 VALUES ('Burger Two', FALSE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten) 
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten) 
 VALUES ('Burger Three', TRUE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten)
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten)
 VALUES ('Burger Four', FALSE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten) 
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten) 
 VALUES ('Burger Five', TRUE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten)
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten)
 VALUES ('Burger Six', FALSE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten) 
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten) 
 VALUES ('Burger Seven', TRUE);
-INSERT INTO pknrs7m4w9udqng2.Burgers (BurgerDesc, Eaten)
+INSERT INTO kv0muyhjf4teca6w.Burgers (BurgerDesc, Eaten)
 VALUES ('Burger Eight', FALSE);
